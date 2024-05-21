d819c49ef3000a2b1644a3e44e2f265a361e9054
Danilla <122106121+danilla-dev@users.noreply.github.com>
2024-05-21 16:33:58 +0200

    Update useLogin.jsx

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -10,7 +10,7 @@ const useLogin = () => {
 		setIsLoading(true)
 		setError(null)
 
-		const response = await fetch('/api/user/login', {
+		const response = await fetch('https://stocklevels-d7391a6e76fd.herokuapp.com/api/user/login', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ email, password }),
